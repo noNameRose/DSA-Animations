a3da5057c078826bb9a32a43afb2910745983c8e
Dat Nguyen <nguy5079@umn.edu>
2025-01-21 05:30:05 -0600

    Add my name at footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto p-[5em] text-[#e8e8e8] flex flex-col items-center gap-[1em] justify-start">
         <h1 className="font-bold">DSA ANIMATION</h1>
         <p className="text-center">Copyright &copy; 2025 DSA ANIMATION. All Right reserved</p>
+        <p>Created By <span className="font-bold">Dat Nguyen</span></p>
       </div>
     </footer>
   )
